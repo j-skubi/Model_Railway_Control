143205992afc87f53973c430aadc5ca124ee5059
Julian <julian.skublics@tum.de>
2024-08-10 09:00:18 +0200

    sync commit

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -8,10 +8,12 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class Server {
     private final ThreadPoolExecutor threadPool;
     private final Layout model;
+    private final ClientHandler clientHandler;
 
     public Server (int port) {
         threadPool = (ThreadPoolExecutor) Executors.newCachedThreadPool();
         model = new Layout();
+        clientHandler = new ClientHandler();
     }
 
 }
